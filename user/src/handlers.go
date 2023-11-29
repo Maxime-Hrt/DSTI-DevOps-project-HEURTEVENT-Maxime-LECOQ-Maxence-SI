5e6482959ac42de85ae84c0b9463b65f08a7575b
MaxenceLecoq921 <maxencelecoqpro@gmail.com>
2023-11-29 13:20:27 +0100

    feat: added swagger for our app

--- user/src/handlers.go
+++ user/src/handlers.go
@@ -6,6 +6,14 @@ import (
 
 var redisService = NewRedisService(Rdb)
 
+// GetContacts godoc
+// @Summary Get list of contacts
+// @Description Retrieve a list of all contacts
+// @Tags contacts
+// @Accept json
+// @Produce json
+// @Success 200 {array} Contact
+// @Router /contacts [get]
 func GetContacts(c *fiber.Ctx) error {
 	contacts, err := redisService.GetAllContactsFromRedis()
 	if err != nil {
@@ -14,6 +22,17 @@ func GetContacts(c *fiber.Ctx) error {
 	return c.JSON(contacts)
 }
 
+// CreateContact godoc
+// @Summary Create a new contact
+// @Description Add a new contact to the database
+// @Tags contacts
+// @Accept json
+// @Produce json
+// @Param contact body Contact true "Contact to add"
+// @Success 201 {object} Contact "Contact successfully created"
+// @Failure 400 {string} string "Invalid input"
+// @Failure 500 {string} string "Internal server error"
+// @Router /contacts [post]
 func CreateContact(c *fiber.Ctx) error {
 	var contact Contact
 	if err := c.BodyParser(&contact); err != nil {
@@ -27,6 +46,16 @@ func CreateContact(c *fiber.Ctx) error {
 	return c.Status(201).JSON(contact)
 }
 
+// GetContact godoc
+// @Summary Get a contact by ID
+// @Description Retrieve a contact by its unique ID
+// @Tags contacts
+// @Accept json
+// @Produce json
+// @Param id path string true "Contact ID"
+// @Success 200 {object} Contact "Contact found"
+// @Failure 404 {string} string "Contact not found"
+// @Router /contacts/{id} [get]
 func GetContact(c *fiber.Ctx) error {
 	id := c.Params("id")
 
@@ -38,6 +67,18 @@ func GetContact(c *fiber.Ctx) error {
 	return c.JSON(contact)
 }
 
+// UpdateContact godoc
+// @Summary Update a contact
+// @Description Update an existing contact by its unique ID
+// @Tags contacts
+// @Accept json
+// @Produce json
+// @Param id path string true "Contact ID"
+// @Param contact body Contact true "Updated contact information"
+// @Success 200 {object} Contact "Contact successfully updated"
+// @Failure 400 {string} string "Invalid input"
+// @Failure 500 {string} string "Internal server error"
+// @Router /contacts/{id} [put]
 func UpdateContact(c *fiber.Ctx) error {
 	id := c.Params("id")
 
@@ -53,6 +94,16 @@ func UpdateContact(c *fiber.Ctx) error {
 	return c.Status(200).JSON(contact)
 }
 
+// DeleteContact godoc
+// @Summary Delete a contact by ID
+// @Description Remove a contact from the database by its unique ID
+// @Tags contacts
+// @Accept json
+// @Produce json
+// @Param id path string true "Contact ID"
+// @Success 204 "Contact successfully deleted"
+// @Failure 500 {string} string "Internal server error"
+// @Router /contacts/id/{id} [delete]
 func DeleteContact(c *fiber.Ctx) error {
 	id := c.Params("id")
 
@@ -63,6 +114,16 @@ func DeleteContact(c *fiber.Ctx) error {
 	return c.SendStatus(204)
 }
 
+// DeleteContactByEmail godoc
+// @Summary Delete a contact by email
+// @Description Remove a contact from the database by email
+// @Tags contacts
+// @Accept json
+// @Produce json
+// @Param email path string true "Contact Email"
+// @Success 204 "Contact successfully deleted"
+// @Failure 500 {string} string "Internal server error"
+// @Router /contacts/email/{email} [delete]
 func DeleteContactByEmail(c *fiber.Ctx) error {
 	email := c.Params("email")
 
